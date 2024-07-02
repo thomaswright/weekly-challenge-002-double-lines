c8d05d44c803ffa274937bf353385b15a1de39c1
Thomas Wright <thomaswright1@gmail.com>
2024-07-01 19:35:46 -0500

    update readme

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://thomaswright.github.io/weekly-challenge-002-double-lines/",
   plugins: [
     react({
       include: ["**/*.res.mjs"],
